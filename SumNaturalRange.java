1ebd8eba5c49d086be5cb76a3d94bbc007b37da7
ar25029 <ar25029@gmail.com>
2025-05-17 20:39:06 +0530

    new Code added

--- SumNaturalRange.java
+++ SumNaturalRange.java
@@ -1,8 +1,16 @@
 public class SumNaturalRange {
 
     public static int sumNatural(int n1, int n2){
-        return (n2*(n2+1)/2) - (n1 * (n1+1)/2);
+        return (n2*(n2+1)/2) - (n1 * (n1-1)/2);
     }
+
+    // public static int sumNatural(int n1, int n2){
+    //     int sum = 0;
+    //     for(int i=n1; i<=n2; i++){
+    //         sum+=i;
+    //     }
+    //     return sum;
+    // }
     public static void main(String[] args) {
         int n1 = Integer.parseInt(args[0]);
         int n2 = Integer.parseInt(args[1]);

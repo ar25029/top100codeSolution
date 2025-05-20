de624e2f5614ab83c6b783e28c7962af398d1a3e
ar25029 <ar25029@gmail.com>
2025-05-20 10:58:04 +0530

    new code added

--- ReverseDigit.java
+++ ReverseDigit.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class ReverseDigit {
 
     public static int reverseNumber(int n){
@@ -12,9 +10,7 @@ public class ReverseDigit {
         return rev;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
+        int n = Integer.parseInt(args[0]);
         System.out.println(reverseNumber(n));
     }
 }
